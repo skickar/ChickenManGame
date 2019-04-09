b2d22759dcff779546aca919db298e1fab7ea3b7
Skickar <40251293+skickar@users.noreply.github.com>
2019-04-08 17:15:40 -0700

    Update EGG.ino

--- EGG.ino
+++ EGG.ino
@@ -18,53 +18,55 @@ int ledGreen = D3;
 int laserPin = D8; // This one fires the laser
 int delayTime = 20; // Adjust the time of delay
 int highValue = 90; // Defines the highest brightness for analog values (makes sure the LED's don't get too bringt)
-int level = 0;
+int level = 0; 
 
 const char* easyPass[] PROGMEM = {
-"password"
-"123456789"
-"12345678"
-"1q2w3e4r"
-"sunshine"
-"football"
-"1234567890"
-"computer"
-"superman"
-"internet"
-"iloveyou"
-"1qaz2wsx"
-"baseball"
-"whatever"
-"princess"};
+"password\0",
+"123456789\0",
+"12345678\0",
+"1q2w3e4r\0",
+"sunshine\0",
+"football\0",
+"1234567890\0",
+"computer\0",
+"superman\0",
+"internet\0",
+"iloveyou\0",
+"1qaz2wsx\0",
+"baseball\0",
+"whatever\0",
+"princess\0"};
 
 const char* numberAdd[] PROGMEM = {
-"One"
-"Two"
-"Three"
-"Four"
-"Five"
-"Six"
-"Seven"
-"Eight"
-"Nine"
-"Ten"
-"Eleven"
-"Twelve"
-"Thirteen"
-"Fourteen"
-"Fifteen"};
+"One\0",
+"Two\0",
+"Three\0",
+"Four\0",
+"Five\0",
+"Six\0",
+"Seven\0",
+"Eight\0",
+"Nine\0",
+"Ten\0",
+"Eleven\0",
+"Twelve\0",
+"Thirteen\0",
+"Fourteen\0",
+"Fifteen\0"};
 
 
 void createBird(){
 int randNumber = random(0, 14);
+Serial.print("Random Number:");
+Serial.println(randNumber);
 char ChickenNumber = char(randNumber);
 const char* password = easyPass[randNumber];
 const char* ssid = numberAdd[randNumber];
+Serial.print("Creating the bird with SSID ");Serial.print(ssid);Serial.print(" and password ");Serial.println(password);
  switch(level){
   case 0:
- Serial.print("Setting soft-AP with ");
-  Serial.print(ChickenNumber);
- Serial.println(WiFi.softAP('ChickenEasy'+ssid, password) ? "Ready" : "Failed!");
+ Serial.println("Setting soft-AP:");
+ Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
     // statements
     break;
   case 1:
@@ -100,16 +102,15 @@ void blinkLed(int currentLed, int numbBlinks){
 }
 // This pulses the laser for zero to full intensity. 
 void pulseLaser(){
-delay(1000);
 for (int i = 0; i < 255; i ++){
   analogWrite(laserPin, i);
   delay(5);}
    analogWrite(laserPin, 0);
-delay(1000);
 }
 
 void setup() {
   // Set all LED pins to output
+  Serial.begin(9600);
   pinMode(ledRed, OUTPUT);
   pinMode(ledBlue, OUTPUT);
   pinMode(ledGreen, OUTPUT);
@@ -117,13 +118,15 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output to turn it the hell off
   digitalWrite(LED_BUILTIN, HIGH);  // onboard LED OFF get the heck out
   randomSeed(analogRead(0));
+  delay(3000);
   createBird();
   }
 
 void loop() { // Testing here
-blinkWhite(10);
-blinkLed(ledRed, 10);
-blinkLed(ledBlue, 10);
-blinkLed(ledGreen, 10);
+//blinkWhite(10);
+//blinkLed(ledRed, 10);
+//blinkLed(ledBlue, 10);
+//blinkLed(ledGreen, 10);
+Serial.println("Loop is running");
 pulseLaser();
 }
