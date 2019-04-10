02c9b7b55705c1c99282a3e8246657be2cb05410
Brandon Paiz <paiz.brandon@gmail.com>
2019-04-09 18:41:23 -0700

    mystery feature

--- CHICK.ino
+++ CHICK.ino
@@ -118,20 +118,22 @@ void handleRoot() {
         "<!DOCYTPE html>"
         "<html>"
         "<head>"
-        "<meta charset=\"utf-8\">"
-        "<title>SimpleCLI</title>"
+          "<meta charset=\"utf-8\">"
+          "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+          "<title>LAY YOUR EGG</title>"
         "</head>"
-        "<body>"
-        "<p>";
+        "<body style=\"display:flex; flex-direction:column;justify-content:center;align-items:center;height:100%; background-color:black;\">"
+          "<style>"
+            "a.button{display:inline-block;padding:.7em 1.4em;margin:0 .3em .3em 0;border-radius:.15em;box-sizing:border-box;text-decoration:none;font-family:Roboto,sans-serif;text-transform:uppercase;font-weight:400;color:#fff;width:30%;box-shadow:inset 0 -.6em 0 -.35em rgba(0,0,0,.17);text-align:center;position:relative}a.button:active{top:.1em}@media all and (max-width:500px){a.button{display:block;width:50%;margin:.4em auto}}"
+          "</style>"
+          "<a href=\"index.html?cmd=led+red\" class=\"button\" style=\"background-color:#f8333c\">Red Team</a>"
+          "<a href=\"index.html?cmd=led+green\" class=\"button\" style=\"background-color:#44af69\">Green Team</a>"
+          "<a href=\"index.html?cmd=led+blue\" class=\"button\" style=\"background-color:#3369ff\">Blue Team</a>"
+          "<p>";
 
-    html += answer;
-
-    html +=
-        "</p>"
-        "<form action=\"index.html\" method=\"GET\">"
-        "<input type=\"text\" name=\"cmd\"></input>"
-        "<input type=\"submit\"></input>"
-        "</form>"
+          html += answer;
+          html +=
+          "</p>"
         "</body>"
         "</html>";
 
