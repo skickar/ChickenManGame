b3725bb4a19b58e3b785be1bb8cb0eaff084fb61
Brandon Paiz <paiz.brandon@gmail.com>
2019-04-13 19:50:18 -0700

    make numbers not retarded

--- constants.h
+++ constants.h
@@ -62,21 +62,22 @@ const char* hardPass[] PROGMEM = {
 };
 
 const char* numberAdd[] PROGMEM = {
-	"One\0",
-	"Two\0",
-	"Three\0",
-	"Four\0",
-	"Five\0",
-	"Six\0",
-	"Seven\0",
-	"Eight\0",
-	"Nine\0",
-	"Ten\0",
-	"Eleven\0",
-	"Twelve\0",
-	"Thirteen\0",
-	"Fourteen\0",
-	"Fifteen\0"
+	"00\0",
+	"01\0",
+	"02\0",
+	"03\0",
+	"04\0",
+	"05\0",
+	"06\0",
+	"07\0",
+	"08\0",
+	"09\0",
+	"10\0",
+	"11\0",
+	"12\0",
+	"13\0",
+	"14\0",
+	"15\0"
 };
 
 #endif
