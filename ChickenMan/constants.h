7680133e8695806e24f90e83e1232f6c4d592dee
Stefan Kremser <stef.1497@gmail.com>
2019-04-20 11:45:09 +0200

    Updated Chicken man and added missing files

---
+++ ChickenMan/constants.h
@@ -0,0 +1,93 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int NUM_PASSWORDS = 15;
+
+const char prefix0[] = "Chicken_Easy_";
+const char prefix1[] = "Chicken_Medium_";
+const char prefix2[] = "Chicken_Hard_";
+const char prefix3[] = "CHICKEN_COOKED_";
+
+const char SUPER_SECRET[] = "edfd8e8160383696120dfb444a8b43f1";
+
+const int EASY_POINTS_PER_SECOND   = 1;
+const int MEDIUM_POINTS_PER_SECOND = 2;
+const int HARD_POINTS_PER_SECOND   = 5;
+
+// how often to increment team points by [DIFF]_POINTS_PER_SECOND
+const int POINT_INTERVAL_SECONDS = 1;
+
+const char* easyPass[] PROGMEM = {
+    "password\0",
+    "123456789\0",
+    "12345678\0",
+    "1q2w3e4r\0",
+    "sunshine\0",
+    "football\0",
+    "1234567890\0",
+    "computer\0",
+    "superman\0",
+    "internet\0",
+    "iloveyou\0",
+    "1qaz2wsx\0",
+    "baseball\0",
+    "whatever\0",
+    "princess\0"
+};
+
+const char* mediumPass[] PROGMEM = {
+    "sweetheart\0",
+    "overlord\0",
+    "michaela\0",
+    "meredith\0",
+    "buttercup\0",
+    "abc12345\0",
+    "aardvark\0",
+    "Passw0rd\0",
+    "12345678910\0",
+    "universal\0",
+    "trinidad\0",
+    "thursday\0",
+    "standard\0",
+    "pearljam\0",
+    "anonymous\0"
+};
+
+const char* hardPass[] PROGMEM = {
+    "prairie chicken\0",
+    "chicken-breasted\0",
+    "chicken broth\0",
+    "chicken cholera\0",
+    "chickenpox\0",
+    "chicken pox\0",
+    "chicken roost\0",
+    "Chicken scratch\0",
+    "chicken septicemia\0",
+    "CHICKEN SHACK\0",
+    "chicken snake\0",
+    "chickensoup\0",
+    "chicken-spirited\0",
+    "chickens-toes\0",
+    "chicken thief\0"
+};
+
+const char* numberAdd[] PROGMEM = {
+    "00\0",
+    "01\0",
+    "02\0",
+    "03\0",
+    "04\0",
+    "05\0",
+    "06\0",
+    "07\0",
+    "08\0",
+    "09\0",
+    "10\0",
+    "11\0",
+    "12\0",
+    "13\0",
+    "14\0",
+    "15\0"
+};
+
+#endif /* ifndef CONSTANTS_H */
